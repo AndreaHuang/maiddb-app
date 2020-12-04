95a0c1b341d4876d2eae20b3c6caa8499db98bd0
Andrea Huang <hyfandrea@gmail.com>
2020-12-04 11:02:06 +0800

    activity indicator overlay

--- app/components/ActivityIndicator.js
+++ app/components/ActivityIndicator.js
@@ -1,14 +1,37 @@
 import React from "react";
+import {StyleSheet,Modal,View} from "react-native";
 import LottieView from "lottie-react-native";
+import {Overlay } from 'react-native-elements';
+
+import color from "../config/color";
+
 function ActivityIndicator({ visible }) {
   if (!visible) return null;
   return (
+    <Overlay  isVisiable={visible} 
+    overlayStyle={styles.overlay} 
+    fullScreen={true}>
+      
     <LottieView
       autoPlay
       loop
       source={require("../assets/animations/loader.json")}
     />
+    </Overlay>
   );
 }
+const styles=StyleSheet.create({
+   overlay: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: color.white,
+    opacity: 0.5,
+  }
+})
 
 export default ActivityIndicator;
